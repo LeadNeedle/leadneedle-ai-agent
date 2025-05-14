df8e1226cdb4fa4e530a778c649efd3b9bd2febd
Dylan Spencer <dylan@leadneedle.com>
2025-05-14 18:32:24 -0400

    Add privacy policy page and route

--- website.py
+++ website.py
@@ -7,3 +7,7 @@ website_bp = Blueprint('website_bp', __name__, template_folder='templates')
 @website_bp.route('/')
 def home():
     return render_template("index.html")
+
+@website_bp.route('/privacy-policy')
+def privacy_policy():
+    return render_template("privacy-policy.html")

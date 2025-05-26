35afd0f24bc476773094d06265c2c6ba77055889
Dylan Spencer <dylan@leadneedle.com>
2025-05-26 02:02:51 -0400

    gitignore and manual py thing

---
+++ backend/manual_auth.py
@@ -0,0 +1,27 @@
+import gspread
+import pickle
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+def main():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('leadneedle_auth.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    client = gspread.authorize(creds)
+    sheet = client.open("Lead Needle Contacts").worksheet("Submissions")
+    sheet.append_row(["Test Auth", "Done"])
+
+if __name__ == '__main__':
+    main()

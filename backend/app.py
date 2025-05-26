4387c521c004f07a939249d9c83933712c6b1339
Dylan Spencer <dylan@leadneedle.com>
2025-05-26 01:55:15 -0400

    fixed get google sheets

--- backend/app.py
+++ backend/app.py
@@ -21,11 +21,28 @@ app.register_blueprint(website_bp)
 
 # Google Sheets setup
 def get_google_sheet():
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    creds = ServiceAccountCredentials.from_json_keyfile_name('backend/leadneedle_auth.json', scope)
+    import pickle
+    from google_auth_oauthlib.flow import InstalledAppFlow
+    from google.auth.transport.requests import Request
+
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+    creds = None
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('backend/leadneedle_auth.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
     client = gspread.authorize(creds)
-    sh = client.open("Lead Needle Contacts").worksheet("Submissions")
-    return sh
+    return client.open("Lead Needle Contacts").worksheet("Submissions")
 
 # Email notification function
 def send_notification_email(form_data):
